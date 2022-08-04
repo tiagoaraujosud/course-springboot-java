2aa6681b340239526965bb502824cffbf186ea38
Tiago Araujo <tiagoaraujosud@gmail.com>
2022-08-04 10:15:46 -0300

    Create User Resource

--- src/main/java/com/estudospring/course/resources/UserResource.java
+++ src/main/java/com/estudospring/course/resources/UserResource.java
@@ -1,5 +1,20 @@
 package com.estudospring.course.resources;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudospring.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Araujo", "tiagoaraujo@gmail.com", "9999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
 
 }
