893012b989765fa78d25d9b133b13fc53c75a961
Tiago Araujo <tiagoaraujosud@gmail.com>
2022-08-09 15:48:20 -0300

    DataBase H2, Test Profile, JPA: Error running DDL FIXED

--- src/main/java/com/estudospring/course/entities/User.java
+++ src/main/java/com/estudospring/course/entities/User.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+@Entity(name="teste")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
