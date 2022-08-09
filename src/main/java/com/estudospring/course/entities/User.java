328633a42f28e4df980a3ec872322fc6703cddcc
Tiago Araujo <tiagoaraujosud@gmail.com>
2022-08-09 16:01:30 -0300

    Change @Entity = usuario

--- src/main/java/com/estudospring/course/entities/User.java
+++ src/main/java/com/estudospring/course/entities/User.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name="teste")
+@Entity(name="usuario")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
